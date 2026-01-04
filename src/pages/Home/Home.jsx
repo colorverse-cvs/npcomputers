03d9deb2b737de1c0774013904cd1228834d79c3
ghodekarpratik200-arch <ghodekarpratik200@gmail.com>
2026-01-05 00:19:46 +0530

    Why choose us section development

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Features from "../../components/Features/Features";
 import Services from "../../components/Services/Services";
+import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 import CTA from "../../components/CTA/CTA";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Features />
       <CTA />
+      <WhyChooseUs/>
       <Services />
     </>
   );
