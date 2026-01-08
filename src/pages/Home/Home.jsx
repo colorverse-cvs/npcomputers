3d5711fb5a57028461353411d2f61dcd283dc610
ghodekarpratik200-arch <ghodekarpratik200@gmail.com>
2026-01-08 23:24:45 +0530

    product carousel section development

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
-import Features from "../../components/Features/Features";
 import Services from "../../components/Services/Services";
 import WhyChooseUs from "../../components/WhyChooseUs/WhyChooseUs";
 import CTA from "../../components/CTA/CTA";
 import AboutUs from "../../components/About/AboutUs";
+import ModelsCarousel from "../../components/ModelsCarousel/ModelsCarousel";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
+      <ModelsCarousel/>
+      {/* <Features /> */}
       <AboutUs/>
-      
       <WhyChooseUs/>
       <Services />
       <CTA />
